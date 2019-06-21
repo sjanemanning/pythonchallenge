61b286a3672df4faaef704c1f386e3c7acda68f3
sjanemanning <sarah.hill.manning@gmail.com>
2019-06-20 23:19:24 -0500

    Final2

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,29 +1,48 @@
 import os
 import csv
+import sys
+sys.stdout = open('solutions_pypoll.txt' , 'w' )
 
 pypoll_path = os.path.join('Resources','election_data.csv')
 
 countofvotes=0
 candidate = ''
-candidatevotes = {}
+candidatelist = {}
+votepercent = {}
+mostvotes = 0
+winner = ''
+
 
 
 with open(pypoll_path,'r', newline="") as pypollcsv:
     csvreader = csv.reader(pypollcsv , delimiter=',')
-    
+
     next(csvreader, None)
 
     for row in csvreader:
         countofvotes = countofvotes + 1
         candidate = row[2]
-        if candidate in candidatevotes:
-            candidatevotes[candidate] = candidatevotes[candidate] + 1
+        if candidate in candidatelist:
+            candidatelist[candidate] = candidatelist[candidate] + 1
         else:
-            candidatevotes[candidate] = 1
+            candidatelist[candidate] = 1
+
+for person, votecount in candidatelist.items():
+    votepercent[person] = '{0:.0%}'.format(votecount / countofvotes)
+    if votecount > mostvotes:
+        mostvotes = votecount
+        winner = person
 
 
 
-print(countofvotes)
-print(candidate)
-print(candidatevotes)
+print("Election Results")
+print(f"Total Votes: {countofvotes}")
+for person, votecount in candidatelist.items():
+    print(f"{person}: {votepercent[person]} ({votecount})")
+print(f"Winner: {winner}")
+
+
+
+    
+
 
